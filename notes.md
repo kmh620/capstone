2615acf0745824d9ef9d72b05d5151af7f123cab
Katrina Hockman <khockman90@hotmail.com>
2019-03-01 11:01:14 -0800

    Research react/ how to best use components, add basic form info

--- notes.md
+++ notes.md
@@ -2,3 +2,8 @@
 
 ![alt text](src/assets/img/CapstoneLayout1.png "MVP web layout")
 
+### Notes:
+
+* [Chatroom App tutorial(for advanced feature):](https://www.freecodecamp.org/news/full-react-course-how-to-build-a-chat-room/)
+
+
---
+++ src/components/newproject/OtherForm.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function OtherForm(){
+  let _title = null;
+  let _medium = null;
+  let _notes = null;
+  let _dimensions = null;
+
+  function handleOtherFormSubmission(event) {
+    event.preventDefault();
+    _title.value = ''
+    _medium.value = ''
+    _notes.value = ''
+    _dimensions.value = ''
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleOtherFormSubmission}>
+        <input
+          type='text'
+          id='title'
+          placeholder='Title'
+          ref={(input) => {_title = input;}}/>
+        <input
+          type='text'
+          id='medium'
+          placeholder='Location'
+          ref={(input) => {_medium = input;}}/>
+          <input
+            type='text'
+            id='dimensions'
+            placeholder='HxLxW'
+            ref={(input) => {_dimensions = input;}}/>
+        <textarea
+          id='notes'
+          placeholder='Notes'
+          ref={(textarea) => {_notes = textarea;}}/>
+        <button type='submit'>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default OtherForm;
