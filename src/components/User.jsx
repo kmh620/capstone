d11bd133b83e86a55f5ee90b6249fde81275bc05
Katrina Hockman <khockman90@hotmail.com>
2019-03-01 15:14:09 -0800

    Add lists to user page

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,13 +2,14 @@
 import React from 'react';
 
 import ProjectTypeList from './projects/ProjectTypeList';
-
+import NewProjectControl from './newproject/NewProjectControl';
 import SeriesList from './SeriesList';
 
 
 function User(){
   return (
     <div>
+      <NewProjectControl/>
       <ProjectTypeList/>
       <SeriesList/>
     </div>
--- src/components/projects/ProjectTypeList.jsx
+++ src/components/projects/ProjectTypeList.jsx
@@ -1,11 +1,26 @@
 
 import React from 'react';
+import PaintList from './PaintList';
+import SculptureList from './SculptureList';
+import OtherList from './OtherList';
 
 
 function ProjectTypeList(){
   return (
     <div>
       <h1>Project by type List</h1>
+      <div>
+        <h2>painting</h2>
+        <PaintList/>
+      </div>
+      <div>
+        <h2>sculpture</h2>
+        <SculptureList/>
+      </div>
+      <div>
+        <h2>Other</h2>
+        <OtherList/>
+      </div>
     
     </div>
   );
