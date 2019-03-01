070ac7e1433eeeaefad5de848240129bf7b24e03
Katrina Hockman <khockman90@hotmail.com>
2019-03-01 10:02:55 -0800

    Add basic main page,

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Main from './Main';
 
 
-class App extends Component {
-  render() {
+function App() {
+  
     return (
       <div className="App">
-        <header className="App-header">
-        
-        <h1>Hello</h1>
-        </header>
+        <Main/>
       </div>
     );
   }
-}
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Main(){
+  return (
+    <div>
+      <h3>Main App Page</h3>
+      <p><em>About</em></p>
+      <p><em>Login</em></p>
+      <p><em>Create Account</em></p>
+    </div>
+  );
+}
+
+export default Main;
