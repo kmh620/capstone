7dedcb10f052a2a80616687d0f3e4a45a3df4235
Katrina Hockman <khockman90@hotmail.com>
2019-03-08 11:24:33 -0800

    Add new single form, look up css options

---
+++ src/components/ProjectForm.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+
+function Form(props) {
+  let _title = null;
+  let _medium = null;
+  let _size = null;
+
+
+  function handleNewFormSubmission(event) {
+    event.preventDefault();
+    props.onNewActorCreation({title: _title.value, medium: _medium.value, size: _size.value, id: v4()});
+    _title.value = '';
+    _medium.value = '';
+    _size.value = '';
+  }
+
+
+  return(
+    <div>
+      <form onSubmit={handleNewFormSubmission}>
+        <input type="text" placeholder="Title" ref={(input) => {_title = input;}}/>
+        <input type="text" placeholder="Medium"ref={(input) => {_medium = input;}}/>
+        <input type="text" placeholder="Size"ref={(input) => {_size = input;}}/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
